61e031fdd8d621cb183caaea07133bb346310892
Bryan Phelps <bryphe@outrunlabs.com>
2020-01-16 17:15:08 -0800

    Bugfix: Incorrect 'lib' dir on CentOS
    Issue: When building on CentOS 7, -ljpeg could not be found.
    
    Defect: The libraries were being put in $prefix/lib64 instead of $prefix/lib
    
    Fix: Specify lib directory

--- esy-build.sh
+++ esy-build.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 
-cmake -G"Unix Makefiles" -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$cur__install -S $cur__root -B $cur__target_dir
+cmake -G"Unix Makefiles" -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$cur__install -DCMAKE_INSTALL_DEFAULT_LIBDIR=lib -S $cur__root -B $cur__target_dir
 make -C $cur__target_dir
 make -C $cur__target_dir install
